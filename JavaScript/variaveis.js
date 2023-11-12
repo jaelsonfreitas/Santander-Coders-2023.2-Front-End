baaaeb4f7c3aa5e3358f875204a28d32b4a25877
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-11 21:08:46 -0300

    Variaveis.

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,12 @@
+let idade = 33
+let nome = 'Jaélson '
+let firstName = 'José de Freitas'
+
+console.log("Olá seu nome é: " + nome + firstName + " e você tem " + idade + " anos." );
+let masc = true
+console.log("Seu Sexo e masculino? " + masc);
+/* se for masculino imprime vedadeiro.*/
+
+let outra;
+
+
