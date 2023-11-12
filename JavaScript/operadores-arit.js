e690de25145f8212c8879f2bb3ff9c99a998c2cc
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-11 21:36:20 -0300

    Operadores aritméticos.

---
+++ JavaScript/operadores-arit.js
@@ -0,0 +1,41 @@
+let number1 = 20;
+let number = 5;
+
+//Adicão
+let soma = number + number1
+console.log('A Soma total é: ' + soma)
+
+//Subtração
+
+let subtracao = number1 - number
+
+console.log('A subtração de 20 e 5 é igual á: ' + subtracao)
+
+// multiplicação
+ let multiplicação = number * number1
+ console.log('A multiplicação de 20 e 5 é igual á: ' + multiplicação)
+
+ //divisão
+
+ let divisão = number1 / number
+
+ console.log('A divisão de 20 e 5 é igual á: ' + divisão)
+
+ //exponenciacão
+
+ let exponenciacão = number1 ** number
+
+ console.log('O exponencial de 20 e 5 é igual á: ' + exponenciacão)
+ //Resto da divisão
+
+
+ let resto = number1 % number
+
+ console.log(' O resto da divisào de 20 e 5 é igual á: ' + resto)
+
+ console.log( ((3 + 7) * 3) - 30);
+
+ // Biblioteca Math
+
+ console.log("O Valor de PI é: " + Math.PI);
+ console.log("A Raiz quadrada de " + soma + " é : " + Math.sqrt(soma));
