73066038b47f9aa5ff0d5c3cc2d94cfc1258683b
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-12 08:47:30 -0300

    resolver problemas com modulos.

---
+++ JavaScript/lacos_numericos.js
@@ -0,0 +1,14 @@
+// LAÇOS NUMERICOS : POR
+const input = require('readline-sync')
+
+//O problema.
+//Pegar 3 notas de um aluno e imprimir a média.
+const  nota1 = Number(input.question("Informe a primeira nota: "))
+const  nota2 = Number(input.question("Informe a segunda nota: "))
+const  nota3 = Number(input.question("Informe a terceira nota: "))
+
+
+// Acumulador
+
+
+// Estrutura for
