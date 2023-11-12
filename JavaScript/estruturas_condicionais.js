334257cb1abc2eb17747b94347dee2003b4d9e0c
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-11 23:12:11 -0300

    Estruturas condicionais.

---
+++ JavaScript/estruturas_condicionais.js
@@ -0,0 +1,20 @@
+const idade = 24;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+}else{
+    console.log("Você é menor de idade");
+};
+
+// Se média >= 7, aprovado
+// Se média <7 e media >= 5, recuperação
+// Se média < 5, reprovado
+
+let media = 6.9
+if ( media >= 7) {
+    console.log("Aprovado");
+} else if ( media >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
