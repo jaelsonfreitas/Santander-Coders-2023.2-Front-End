b607fd19de25d288f10e53821dc4bd1c9ebbe2d1
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-13 16:32:35 -0300

    Ajustes.

---
+++ JavaScript/arrays_p2.js
@@ -0,0 +1,16 @@
+// > ARRAYS - PARTE 2 - MÉTODOS DE ARRAYS
+
+let arr1 = [30, 12, 45, 34, 29]
+let arr2 = []
+
+// fatiamento Slice
+
+console.log(arr1.slice(0, 2));
+
+// adicionando elementos: push | unshift
+
+console.log('Antes de adicionar: ', arr2);
+arr2.push('Jose', 'Maria', 10, 55, 1.71, 'Freitas', 'Fidelis');
+arr2.push('Moreira')
+arr2.unshift('Pedro')
+console.log('Depois de Adcionar: ', arr2);
